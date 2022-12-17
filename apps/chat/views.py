2b100a42268f538ad181054bad4ad31d5d91efe8
Caipo <Demetrick@Nicholass-MacBook-Pro-2.local>
2022-12-16 19:46:20 -0500

    pages now are extended from base

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
 def app_index(request):
    return render(request, 'chat.html')
 
 def room_name(request):
         name = request.path_info.split(r'/')[-3]
-        print('asdf')
-        return render(request, 'chat.html', {'room_name': name})
+        return render(request, 'chat.html', {'room_name': name, 'user': request.user})
 
